468141e2612a605d319dd2c3d71482a80c524cfd
connersiou <connersiou@gmail.com>
2025-01-11 12:13:19 -0800

    changed components name

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Header} from './components';
 
 function App() {
   return (
     <div className="App">
+      {/* <Header />
+      <Button /> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ my-app/src/componentstemp/Header/index.js
@@ -0,0 +1,9 @@
+// src/components/Header/index.js
+
+function Header() {
+    return (
+        <h1>Welcome to our website!</h1>
+    );
+}
+
+export default Header;
